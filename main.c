ad3b034b608b59ea756b9eb655fd553c9842b5a9
Alex Yang <ayyang9@gmail.com>
2020-04-22 00:00:23 -0700

    Set up level for screen boundary testing

--- main.c
+++ main.c
@@ -145,7 +145,8 @@ bool should_quit = false;
 void init() {
     gettimeofday(&tv, NULL);
     srand((uint64_t)tv.tv_sec * 1000 + (uint64_t)tv.tv_usec / 1000);
-    float start_x = rand_range(128.0f, screen_width - 128.0f);
+    //float start_x = rand_range(128.0f, screen_width - 128.0f);
+    float start_x = 34.0f;
     float start_y = 128.0f;
 
     ball = (body_t){
@@ -162,10 +163,10 @@ void init() {
 
     bricks[0].x = start_x - brick_width / 2.0f;
     bricks[0].y = start_y;
-    bricks[1].x = start_x - brick_width * 3.0f / 2.0f;
-    bricks[1].y = start_y;
-    bricks[2].x = start_x + brick_width / 2.0f;
-    bricks[2].y = start_y;
+    //bricks[1].x = start_x - brick_width * 3.0f / 2.0f;
+    //bricks[1].y = start_y;
+    bricks[2].x = start_x - brick_width * 3.0f / 2.0f + 16.0f;
+    bricks[2].y = start_y + 64.0f;
 
     float last_x = start_x;
     float last_y = start_y;
