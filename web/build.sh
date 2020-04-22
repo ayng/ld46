9f754e196b7d547c1bd7243cb38c930b4aceca01
Alex Yang <ayyang9@gmail.com>
2020-04-22 16:22:43 -0700

    Move web build files to top level

--- web/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-emcc ../main.c -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s USE_SDL_MIXER=2 -s USE_SDL_TTF=2 -s SDL2_IMAGE_FORMATS='["png"]' -o index.html --preload-file ../assets --shell-file shell.html
-
